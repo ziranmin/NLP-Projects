c8b8af96d89a15405df498ad8874c972b4da89a9
ziranmin <35986494+ziranmin@users.noreply.github.com>
2020-01-23 23:27:11 +0800

    Add files via upload

---
+++ Projects.py
@@ -0,0 +1,11 @@
+'''
+Text Classification https://github.com/ziranmin/NLP-Text-Classification
+
+Vector Space Model https://github.com/ziranmin/NLP-Vector-Space-Model
+
+Recurrent Neural Network https://github.com/ziranmin/NLP-Recurrent-Neural-Network
+
+Spanish Named Entity Recognition https://github.com/ziranmin/NLP-Spanish-Named-Entity-Recognition
+
+Twitter Emoji Prediction https://github.com/ziranmin/NLP-Twitter-Emoji-Prediction
+'''
